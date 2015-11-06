d649ca9e647119934c0ceec4f3281560f503004c
woodylewis <woody@woodylewis.com>
2015-11-06 12:19:56 -0500

    Pass scope paramater to directive controller

--- src/js/directives/alpha.js
+++ src/js/directives/alpha.js
@@ -1,15 +1,25 @@
 'use strict';
 
+
 angular
 .module('multiview.alpha', [])
 .directive('wlsAlpha', function() {
 	return {
 		restrict: 'AE',
+		scope: {
+			bundle: '='
+		},
 		templateUrl: 'templates/send-tpl.html',
 		controllerAs: "us",
 		bindToController: true,
-		controller: function($scope) {
+		controller: function($scope, $document) {
 			var vm = this;
+			$document.ready(function() {
+				vm.doc = $document.find('wls-alpha');
+				vm.proxyBundle = vm.doc[0].attributes.bundle.value;
+				console.log(vm.proxyBundle);
+			});
+
 			vm.bundle = {};
 			vm.bundle.show = 'alpha';
 
