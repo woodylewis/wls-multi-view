68754ceb98d622cdc674d2c35435465715f2d788
woodylewis <woody@woodylewis.com>
2015-11-12 18:14:31 -0500

    Finish first e2e suite

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -15,28 +15,22 @@ describe('wls-multi-views', function() {
 
   it('There should be three send buttons', function() {
     expect(sendButtons.count()).toEqual(3);
-    console.log('3 BUTTONS');
   });
 
   it('Click the third send button', function() {
+    element
+    .all(by.id('alphaDiv'))
+    .getAttribute('bundle')
+    .then( function(data) {
+      console.log('BUNDLE ', data);
+    });
+
     var gamma = sendButtons.get(2)
     gamma.click()
     .then(function() {
       var b1 = element.all(by.css('#b_one'));
-      //var b1 = element.all(by.binding('us.bundle.one')).get(0);
-      //expect(b1.getId()).toEqual('b_one');
-      expect(b1.count()).toEqual(3);
-      /*
       var first = b1.get(0);
-      var theVal = first.getText();
-      console.log('VALUE ', theVal);
-      */
-      element
-      .all(by.id('alphaDiv'))
-      .getAttribute('bundle')
-      .then( function(data) {
-        console.log('BUNDLE ', data);
-      });
+      expect(first.getText()).toContain('gammaOne');
     });
 
   });
